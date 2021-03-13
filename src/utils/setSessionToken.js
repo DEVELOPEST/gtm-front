3a4eb9e232c08be45bc95649bb6ac3f33cc40e2c
mjurge <marten.jurg@hotmail.com>
2021-03-13 11:34:07 +0200

    FRONT-30 Fix cookie adding

--- src/utils/setSessionToken.js
+++ src/utils/setSessionToken.js
@@ -1,15 +1,7 @@
-import {useCookies} from "react-cookie";
 import setAuthHeader from "./setAuthHeader";
 
 export default token => {
-    const [cookies, setCookie, removeCookie] = useCookies();
-    console.log("Set cookie")
-    setCookie('user_jwt', token, {
-        sameSite: 'lax',
-        httpOnly: true,
-        expires: new Date(new Date().getTime() + 300 * 1000),
-        path: '/'
-    });
+    document.cookie = "jwt_token=" + token + "; expires=" + new Date(new Date().getTime() + 300 * 1000) +"; path=/";
     localStorage.setItem('token', token)
     setAuthHeader(token);
 };
