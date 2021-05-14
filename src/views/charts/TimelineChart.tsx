5da5b8b14a17c339906fa3d57e731f02f83c8352
mjurge <marten.jurg@hotmail.com>
2021-05-14 11:20:38 +0300

    FRONT-83 Fix time barchart problems

--- src/views/charts/TimelineChart.tsx
+++ src/views/charts/TimelineChart.tsx
@@ -17,7 +17,7 @@ const TimelineChart = () => {
 
     return (
             <ResponsiveContainer width="100%" height={400} >
-                <ComposedChart data={data} margin={{top: 5, right: 0, left: 0, bottom: 5}}>
+                <ComposedChart  data={data} margin={{top: 5, right: 30, left: 30, bottom: 5}}>
                     <defs>
                         <linearGradient id="colorUvRed" x1="0" y1="0" x2="0" y2="1">
                             <stop offset="95%" stopColor="#b31414" stopOpacity={0.1}/>
@@ -44,7 +44,8 @@ const TimelineChart = () => {
                         axisLine={false}
                         allowDecimals={false}
                     />
-                    <Bar
+                    <Area
+                        type="step"
                         yAxisId="right"
                         dataKey="users"
                         stroke="#f04037"
